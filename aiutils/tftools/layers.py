041bcf7cbf3f5f709f929d1124e72af68d623a58
Tom Le Paine <tom.le.paine@gmail.com>
2016-04-20 11:23:01 -0500

    :memo: Modified doc strings to match Google style guilde.

--- aiutils/tftools/layers.py
+++ aiutils/tftools/layers.py
@@ -12,15 +12,18 @@ def full(input, out_dim, name, gain=np.sqrt(2), func=tf.nn.relu):
     Creates weights and bias parameters with good initial values. Then applies the matmul op and func.
     
     Args:
-      input: A `Tensor` of shape `[batch, in_dim]`.
+      input (tensor): Input to the layer. 
+        Should have shape `[batch, in_dim]`.
         Must be one of the following types: `float32`, `float64`.
-      out_dim: An `int`. Number of output neurons.
-      name: A `string`. Name used by the `tf.variable_scope`.
-      gain: A `float`. Suggest values: sqrt(2) for relu, 1.0 for identity.
-      func: A `function`. 
+      out_dim (int): Number of output neurons.
+      name (string): Name used by the `tf.variable_scope`.
+      gain (float): Gain used when calculating stddev of weights.
+        Suggest values: sqrt(2) for relu, 1.0 for identity.
+      func (function): Function used to calculate neural activations.
       
     Returns:
-      output: A `Tensor` of shape `[batch, in_weight, in_width, out_dim]`.
+      output (tensor): The neural activations for this layer.
+        Will have shape `[batch, out_dim]`.
     """
     in_dim = input.get_shape().as_list()[-1]
     stddev = 1.0 * gain / np.sqrt(in_dim)
@@ -42,21 +45,23 @@ def conv2d(input, filter_size, out_dim, name, strides=[1, 1, 1, 1], padding='SAM
     Creates filter and bias parameters with good initial values. Then applies the conv op and func.
     
     Args:
-      input: A `Tensor` of shape `[batch, in_height, in_width, in_dim]`.
+      input (tensor): Input to the layer. 
+        Should have shape `[batch, in_height, in_width, in_dim]`.
         Must be one of the following types: `float32`, `float64`.
-      filter_size: An `int`. Width and height of square kernel.
-      out_dim: An `int`. Number of output filters.
-      name: A `string`. Name used by the `tf.variable_scope`.
-      strides: A list of `ints`.
-        1-D of length 4.  The stride of the sliding window for each dimension
+      filter_size (int): Width and height of square convolution filter.
+      out_dim (int): Number of output filters.
+      name (str): Name used by the `tf.variable_scope`.
+      strides (List[int]): The stride of the sliding window for each dimension
         of `input`. Must be in the same order as the dimension specified with format.
-      padding: A `string` from: `"SAME", "VALID"`.
+      padding (str): A `string` from: `'SAME', 'VALID'`.
         The type of padding algorithm to use.
-      gain: A `float`. Suggest values: sqrt(2) for relu, 1.0 for identity.
-      func: A `function`. 
+      gain (float): Gain used when calculating stddev of weights.
+        Suggest values: sqrt(2) for relu, 1.0 for identity.
+      func (function): Function used to calculate neural activations.
       
     Returns:
-      output: A `Tensor` of shape `[batch, in_weight, in_width, out_dim]`.
+      output (tensor): The neural activations for this layer.
+        Will have shape `[batch, in_weight, in_width, out_dim]`.
     """
     in_dim = input.get_shape().as_list()[-1]
     stddev = 1.0 * gain / np.sqrt(filter_size*filter_size*in_dim)
