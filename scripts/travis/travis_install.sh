1670262614d172b79185b9de1e77abaa2b629365
Arun Mallya <arun.mallya@gmail.com>
2016-04-21 21:44:49 -0500

    again

--- scripts/travis/travis_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script must be run with sudo.
-
-# Ubuntu/Linux 64-bit, GPU enabled. Requires CUDA toolkit 7.5 and CuDNN v4.  For
-# other versions, see "Install from sources" below.
-pip install --upgrade https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.8.0-cp27-none-linux_x86_64.whl
-
