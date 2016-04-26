076b64815da893d968368a004af6c21652d0a3b9
Tom Le Paine <tom.le.paine@gmail.com>
2016-04-25 22:06:26 -0500

    Add data utils.

---
+++ aiutils/data/data.py
@@ -0,0 +1,35 @@
+import numpy
+from multiprocessing import Process, Queue
+
+
+def sequential(batch_size, num_samples, num_epochs):
+    for epoch in range(num_epochs):
+        indices = numpy.arange(num_samples)
+        for i in range(0, num_samples - batch_size + 1, batch_size):
+            slice_ = slice(i, i+batch_size)
+            yield indices[slice_]
+
+
+def random(batch_size, num_samples, num_epochs):
+    for epoch in range(num_epochs):
+        indices = numpy.random.permutation(num_samples)
+        for i in range(0, num_samples - batch_size + 1, batch_size):
+            slice_ = slice(i, i+batch_size)
+            yield indices[slice_]
+
+
+def batch_generator(data, index_generator, batch_function):
+    for samples in index_generator():
+        batch = data.get(samples)
+        yield batch_function(batch)
+
+
+class BatchFetcher(Process):
+    def __init__(self, queue, batch_generator):
+        self.queue = queue
+        self.batch_generator
+
+    def run(self):
+
+        for batch in batch_generator():
+            self.queue.put(batch)
