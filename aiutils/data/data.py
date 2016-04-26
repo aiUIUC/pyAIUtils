432dc5e552da1b21f0ade731151adc858985fb12
Tom Le Paine <tom.le.paine@gmail.com>
2016-04-25 23:10:06 -0500

    Add addition multiprocessing helper functions. Add corresponding tests.

--- aiutils/data/data.py
+++ aiutils/data/data.py
@@ -1,8 +1,11 @@
+"""Utility functions of making batch generation easier."""
 import numpy as np
 from multiprocessing import Process, Queue
 
 
 def sequential(batch_size, num_samples, num_epochs):
+    """Generate sequence indices.
+    """
     for epoch in range(num_epochs):
         indices = np.arange(num_samples)
         for i in range(0, num_samples - batch_size + 1, batch_size):
@@ -11,6 +14,8 @@ def sequential(batch_size, num_samples, num_epochs):
 
 
 def random(batch_size, num_samples, num_epochs):
+    """Generate random indices.
+    """
     for epoch in range(num_epochs):
         indices = np.random.permutation(num_samples)
         for i in range(0, num_samples - batch_size + 1, batch_size):
@@ -19,20 +24,50 @@ def random(batch_size, num_samples, num_epochs):
 
 
 def batch_generator(data, index_generator, batch_function):
+    """Generate batches of data.
+    """
     for samples in index_generator:
         batch = data.get(samples)
         yield batch_function(batch)
 
 
+def queue_generator(queue, sentinel=None):
+    """Create a generator from a queue.
+    """
+    while True:
+        value = queue.get()
+        if value is not sentinel:
+            yield value
+        else:
+            return
+
+
+def make_queue_generator(data, index_generator, batch_function, queue_maxsize):
+    """Create an asynchronous batch generator.
+    """
+    batcher = batch_generator(data, index_generator, batch_function)
+
+    queue = Queue(maxsize=queue_maxsize)
+    fetcher = BatchFetcher(queue, batcher)
+    fetcher.start()
+
+    queue_batcher = queue_generator(queue)
+    return queue_batcher
+
+
 class BatchFetcher(Process):
     def __init__(self, queue, batch_generator):
+        super(BatchFetcher, self).__init__()
         self.queue = queue
-        self.batch_generator
+        self.batch_generator = batch_generator
 
     def run(self):
 
-        for batch in batch_generator():
+        for batch in self.batch_generator:
             self.queue.put(batch)
+
+        # Signal that fetcher is done.
+        self.queue.put(None)
 
 
 class NumpyData(object):
--- aiutils/data/data_test.py
+++ aiutils/data/data_test.py
@@ -1,5 +1,7 @@
 import numpy as np
 import data
+from multiprocessing import Queue
+
 
 def test_numpy_data_integration():
     temp = np.arange(100)
@@ -8,4 +10,38 @@ def test_numpy_data_integration():
     random_iterator = data.random(10, 100, 2)
     batcher = data.batch_generator(data_, random_iterator, lambda x: x)
     batch = batcher.next()
+    assert batch.shape == (10, 32, 32, 3)
+
+
+def test_batch_fetcher_integration():
+    temp = np.arange(100)
+    array = np.tile(temp[:, None,  None, None], [1, 32, 32, 3])
+    data_ = data.NumpyData(array)
+    random_iterator = data.random(10, 100, 2)
+    batcher = data.batch_generator(data_, random_iterator, lambda x: x)
+
+    queue = Queue(maxsize=10)
+    fetcher = data.BatchFetcher(queue, batcher)
+    fetcher.start()
+
+    queue_batcher = data.queue_generator(queue)
+    # batch = queue_batcher.next()
+
+    for batch in queue_batcher:
+        print batch.shape
+    assert batch.shape == (10, 32, 32, 3)
+
+
+def test_make_queue_generator_integration():
+    temp = np.arange(100)
+    array = np.tile(temp[:, None,  None, None], [1, 32, 32, 3])
+    data_ = data.NumpyData(array)
+    random_iterator = data.random(10, 100, 2)
+    batcher = data.make_queue_generator(data_,
+        random_iterator,
+        lambda x: x,
+        queue_maxsize=10)
+
+    for batch in batcher:
+        print batch.shape
     assert batch.shape == (10, 32, 32, 3)
