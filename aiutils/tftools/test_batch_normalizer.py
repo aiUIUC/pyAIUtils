c748038b297e77bcdff423413cf0c8c3122cf481
Jason <jjrock2@illinois.edu>
2016-04-29 09:54:01 -0500

    update test_batch_normalizer to use graphs local to each test

--- aiutils/tftools/test_batch_normalizer.py
+++ aiutils/tftools/test_batch_normalizer.py
@@ -7,48 +7,78 @@ from batch_normalizer import BatchNormalizer
 from batch_normalizer import batch_normalize
 
 
+@pytest.fixture(scope='function')
+def graph(request):
+    g = tf.Graph()
+    sess = tf.Session(graph=g)
 
-def test_batch_normalize_construct_2():
-    input_shape = [1, 10]
-    x = tf.placeholder(tf.float32, input_shape)
-    ema = tf.train.ExponentialMovingAverage(decay=.9)
-    bn = BatchNormalizer(x, ema, "batch_norm1")
-    assert(bn)
+    def teardown():
+        sess.close()
 
-def test_batch_normalize_construct_4():
-    input_shape = [1, 10, 5, 8]
-    x = tf.placeholder(tf.float32, input_shape)
-    ema = tf.train.ExponentialMovingAverage(decay=.9)
-    bn = BatchNormalizer(x, ema, "batch_norm2")
-    assert(bn)
+    request.addfinalizer(teardown)
 
-def test_batch_normalize_construct_3():
-    with pytest.raises(ValueError):
-        input_shape = [1, 10, 8]
+    return (g, sess)
+
+
+def test_batch_normalize_construct_2(graph):
+    g = graph[0]
+    with g.as_default():
+        input_shape = [1, 10]
         x = tf.placeholder(tf.float32, input_shape)
         ema = tf.train.ExponentialMovingAverage(decay=.9)
-        bn = BatchNormalizer(x, ema, "batch_norm3")
+        bn = BatchNormalizer(x, ema, "batch_norm1")
+        assert(bn)
+
+def test_batch_normalize_construct_4(graph):
+    g = graph[0]
+    with g.as_default():
+        input_shape = [1, 10, 5, 8]
+        x = tf.placeholder(tf.float32, input_shape)
+        ema = tf.train.ExponentialMovingAverage(decay=.9)
+        bn = BatchNormalizer(x, ema, "batch_norm2")
+        assert(bn)
+
+def test_batch_normalize_construct_3(graph):
+    g = graph[0]
+    with g.as_default():
+        with pytest.raises(ValueError):
+            input_shape = [1, 10, 8]
+            x = tf.placeholder(tf.float32, input_shape)
+            ema = tf.train.ExponentialMovingAverage(decay=.9)
+            bn = BatchNormalizer(x, ema, "batch_norm3")
 
 
-def test_batch_normalize():
-    input_shape = [5, 10]
-    x = tf.placeholder(tf.float32, input_shape)
-    ema = tf.train.ExponentialMovingAverage(decay=.9)
-    bn = BatchNormalizer(x, ema, "batch_norm")
+def test_batch_normalize(graph):
+    g = graph[0]
+    with g.as_default():
+        input_shape = [5, 10]
+        x = tf.placeholder(tf.float32, input_shape)
+        ema = tf.train.ExponentialMovingAverage(decay=.9)
+        bn = BatchNormalizer(x, ema, "batch_norm")
 
-    x_val = np.random.rand(*input_shape)
-    normalize = bn.normalize(x)
+        x_val = np.random.rand(*input_shape)
+        normalize = bn.normalize(x)
 
-    sess = tf.Session()
-    sess.run(tf.initialize_all_variables())
-    y = sess.run(normalize, {x:x_val})
-    assert(y.shape == x_val.shape)
+        sess = graph[1]
+        sess.run(tf.initialize_all_variables())
+        y = sess.run(normalize, {x:x_val})
+        assert(y.shape == x_val.shape)
 
-def test_batch_normalize_fcn():
-    input_shape = [5, 10]
-    x = tf.placeholder(tf.float32, input_shape)
-    phase_train = tf.placeholder(tf.bool)
+def test_batch_normalize_fcn(graph):
+    g = graph[0]
+    with g.as_default():
+        input_shape = [5, 10]
+        x = tf.placeholder(tf.float32, input_shape)
+        phase_train = tf.placeholder(tf.bool)
 
-    batch_normalize(x, phase_train, 'batch_norm4')
+        normalize = batch_normalize(x, phase_train, 'batch_norm4')
 
-    assert(len(tf.get_collection('batch_norm_average_update'))==1)
+        assert(len(tf.get_collection('batch_norm_average_update'))==1)
+
+        x_val = np.random.rand(*input_shape)
+
+        sess = graph[1]
+        sess.run(tf.initialize_all_variables())
+        # Just verify that this doesn't error, not really looking for any specific results
+        y1 = sess.run(normalize, {x:x_val, phase_train:True})
+        y2 = sess.run(normalize, {x:x_val, phase_train:False})
