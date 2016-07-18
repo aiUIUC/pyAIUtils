265eab4d26d2091a7a7e35a904ea3da3be5f8f6f
tgupta6 <tgupta6@illinois.edu>
2016-07-18 17:52:17 -0500

    add imresize

--- aiutils/vis/image_io.py
+++ aiutils/vis/image_io.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import math
 import pdb
 
 
@@ -76,3 +77,48 @@ def imwrite(np_im, filename):
         im = Image.fromarray(np_im, 'L')
 
     im.save(filename)
+
+
+def imresize(np_im, method='bilinear', **kwargs):
+    """
+    Matlab like function for resizing image stored as numpy ndarray.
+
+    Args:
+    np_im (numpy.ndarray): h x w x 3 ndarray for color images and 
+        h x w for grayscale images with pixels stored in uint8 format
+    method: Algorithm to use for interpolation. Must be one of
+        {'bilinear', 'nearest', 'lanczos'}
+
+    output_size (int list): output size stored in a list as [h, w]
+    scale (float): scaling factor by which to resize the image. Use 
+        either output_size or scale. Exception is thrown otherwise
+    """
+    assert_str = "Only 1 keyword argument expected with key either" + \
+                 "'output_size' or 'scale'"
+    assert (len(kwargs)==1), assert_str
+
+    if method == 'bilinear':
+        method_ = Image.BILINEAR
+    elif method == 'nearest':
+        method_ = Image.NEAREST
+    elif method == 'lanczos':
+        method = Image.LANCZOS
+    else:
+        assert_str = "Interpolation method must be one of " + \
+                     "{'bilinear', 'nearest', 'lanczos'}"
+        assert(False),  assert_str
+
+    im_h = np_im.shape[0]
+    im_w = np_im.shape[1]
+    if 'output_size' in kwargs:
+        h, w = kwargs['output_size']
+    elif 'scale' in kwargs:
+        h = scale*im_h
+        w = scale*im_w
+    else:
+        assert_str = "Variable argument must be one of {'output_size','scale'}"
+        assert (False), assert_str
+    h = int(math.ceil(h))
+    w = int(math.ceil(w))
+    im = Image.fromarray(np_im)
+    return np.array(im.resize((w,h)))
