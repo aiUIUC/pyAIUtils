ff78d84f119ee398933bfaf81a72e68bdfb8fcb9
tgupta6 <tgupta6@illinois.edu>
2017-02-03 12:30:41 -0600

    modify image_io.imread to read any file format supported by PIL and return im_array (ndarray) and im (PIL object)

--- aiutils/vis/image_io.py
+++ aiutils/vis/image_io.py
@@ -7,18 +7,13 @@ def imread(filename):
     Matlab like function for reading an image file.
 
     Returns:
-    im_array (numpy.ndarray): h x w x 3 ndarray for color images and 
-        h x w for grayscale images
+      im_array (numpy.ndarray): See the following link for information on
+      different modes and the array sizes returned in those modes:
+      http://pillow.readthedocs.io/en/3.4.x/handbook/concepts.html#modes
     """
     im = Image.open(filename)
-
-    err_str = \
-        "imread only supports 'RGB' and 'L' modes, found '{}'".format(im.mode)
-    assert (im.mode == 'RGB' or im.mode == 'L'), err_str
-
     im_array = np.array(im)
-
-    return im_array
+    return im_array, im
 
 
 def imshow(np_im):
@@ -26,8 +21,8 @@ def imshow(np_im):
     Matlab like function for displaying a numpy ndarray as an image
 
     Args:
-    np_im (numpy.ndarray): h x w x 3 ndarray for color images and 
-        h x w for grayscale images with pixels stored in uint8 format
+      np_im (numpy.ndarray): h x w x 3 ndarray for color images and 
+      h x w for grayscale images with pixels stored in uint8 format
     """
     err_str = 'imshow expects ndarray of dimension h x w x c (RGB) or h x w (L)'
     assert (len(np_im.shape) == 3 or len(np_im.shape) == 2), err_str
